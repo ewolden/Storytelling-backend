1d4c636b5b3bde2d9ba3ef78b4e8264b60426647
Eivind H Wolden <ewolden@kjedelig.net>
2015-03-11 13:51:55 +0100

    changed for docker use

--- database/config.php
+++ database/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_USERNAME','');
+define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_HOST','');
-define('DB_NAME','');
+define('DB_NAME','storytelling');
 define('API_URL', 'http://api.digitaltmuseum.no/solr/');
-define('API_KEY', '');
+define('API_KEY', 'demo');
 ?>
