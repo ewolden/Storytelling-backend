40ba8725ff95e807dfcd24b3c1202f4079ce0dd0
audunasa <audunasa@stud.ntnu.no>
2015-04-10 12:02:37 +0200

    merge

---
+++ personalization/weights.php
@@ -0,0 +1,9 @@
+<?php
+define('CATEGORY_LIKE', 0.7);
+define('SUBCATEGORY_LIKE', 0.7);
+define('DISLIKE', 0.3);
+define('SWIPED_PAST', 0.45);
+define('READ_LATER', 0.6);
+define('CATEGORY_NO_ANSWER', 0.3);
+define('SUBCATEGORY_NO_ANSWER', 0.5);
+?>
