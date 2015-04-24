729f1ad767de97a36a5c206bcac62cb760a2ff29
audunasa <audunasa@stud.ntnu.no>
2015-04-24 12:58:35 +0200

    move storysimilarities.php into harvesting.php

--- database/harvesting.php
+++ database/harvesting.php
@@ -5,7 +5,6 @@ Harvests stories from Digitalt fortalt.
 Remember to set constants in the config.php - file
 */
 require_once (__DIR__.'/dbStory.php');
-require_once (__DIR__.'/../personalization/storySimilarities.php');
 
 $dbStory = new dbStory();
 
@@ -13,9 +12,35 @@ $harvestTime = date('m/d/Y h:i:s');
 
 $dbStory->addStoriesToDatabase($harvestTime);
 
+/*Compute similarities between the harvested stories (copied from storySimilarities.php)*/
+$storyArray = $dbStory->getSubcategoriesPerStory();
 $dbStory->close();
 
-/*Compute similarities between the harvested stories*/
-shell_exec('php ../personalization/storySimilarities.php');
+/*Put the computed similarities in this file. Will be overwritten every time this script is run*/
+$simFile = fopen(__DIR__.'/../personalization/similarities.csv', 'w');
+
+/*Looping through to get all possible pairs of stories*/
+for($x=0; $x<sizeof($storyArray); $x++){
+	$firstArray = explode(',',$storyArray[$x]['subcategories']);
+	$firstId = $storyArray[$x]['numericalId'];
+	for($y=$x+1; $y<sizeof($storyArray); $y++){
+		$secondArray = explode(',', $storyArray[$y]['subcategories']);
+		$secondId = $storyArray[$y]['numericalId'];
+		$sim = computeSimilarity($firstArray, $secondArray);
+		$txt = $firstId.",".$secondId.",".$sim."\n";
+		fwrite($simFile,$txt);
+	}
+}
+fclose($simFile);
+
+/*Compute the similarities between stories. Using cosine similarity for now*/
+function computeSimilarity($storyOne, $storyTwo){
+	$storyOneLength = sizeof($storyOne);
+	$storyTwoLength = sizeof($storyTwo);
+	$commonSubCategories = array_intersect($storyOne, $storyTwo);
+	$numCommon = sizeof($commonSubCategories);
+	$cosineSimilarity = $numCommon/(sqrt($storyOneLength)*sqrt($storyTwoLength));
+	return $cosineSimilarity;
+}
 
 ?>
